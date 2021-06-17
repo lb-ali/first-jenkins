3bb0a65803de54e23c4084eca9042882111d3c00
lb-ali <lbalidrive@gmail.com>
2021-06-17 13:12:22 -0400

    Added test.java

---
+++ src/test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("Test ran");
+
+    }
+}
